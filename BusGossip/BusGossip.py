c38a31eaafc09ea8378814085f7fb4a41a9c4e9c
Sam-Hart <sam@samhart.me>
2016-05-03 22:52:52 -0500

    Created drivers and am simulating them traversing their routes

--- BusGossip/BusGossip.py
+++ BusGossip/BusGossip.py
@@ -1,11 +1,50 @@
 class driver:
-	gossips = []
+	_stop = 0
 
-	def __init__(self):
-		self.gossips.append(gossip())
+	def assignRoute(self, route):
+		if not isinstance(route, list):
+			raise TypeError("Route must be a list")
+		self._route = route
 
-class gossip:
-	pass
 
-d = driver()
-print(len(driver.gossips))
+	def currentStop(self):
+		return self._route[self._stop]
+
+	def move(self):
+		self._stop += 1
+		if self._stop >= len(self._route):
+			self._stop = 0
+		
+	
+
+
+	def produceRoute(self):
+		return self._route
+
+
+class simulate:
+	
+	def __init__(self, inputFile):
+		self._inputFile = open(inputFile, 'r')
+		self._drivers = []
+		
+		for line in self._inputFile:
+			line = line.rstrip('\n')	
+			self._drivers.append(driver())
+			self._drivers[-1].assignRoute(line.split(" "))
+		
+		for counter in range(0, 481):
+			for d in self._drivers:
+				
+				d.move()
+
+
+
+			
+
+		
+
+
+
+
+simulate("ExampleInput1.txt")
